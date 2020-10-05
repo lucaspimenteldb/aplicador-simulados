9b68ec730c88729af0180a9587d192772f4feee6
larciu <laerciodasilvapedrosa@gmail.com>
2020-10-05 09:19:06 -0300

    Tirando o botão

--- revisa-3-vue/src/components/dialog/Dialog.vue
+++ revisa-3-vue/src/components/dialog/Dialog.vue
@@ -4,16 +4,16 @@
                 v-model="dialog"
                 width="500"
         >
-            <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                        color="red lighten-2"
-                        dark
-                        v-bind="attrs"
-                        v-on="on"
-                >
-                    Click Me
-                </v-btn>
-            </template>
+<!--            <template v-slot:activator="{ on, attrs }">-->
+<!--                <v-btn-->
+<!--                        color="red lighten-2"-->
+<!--                        dark-->
+<!--                        v-bind="attrs"-->
+<!--                        v-on="on"-->
+<!--                >-->
+<!--                    Click Me-->
+<!--                </v-btn>-->
+<!--            </template>-->
 
             <v-card>
                 <v-card-title
