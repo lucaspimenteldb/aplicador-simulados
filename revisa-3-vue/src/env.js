89485d2cfacdc6150cd04144dc9d66985b10a953
lucasPimentel <lucaspimenteldb@gmail.com>
2020-10-22 09:09:33 -0300

    env

--- revisa-3-vue/src/env.js
+++ revisa-3-vue/src/env.js
@@ -1,5 +1,7 @@
 module.exports = {
   VUE_ENV: 'local',
-  ROOT_API: 'http://178.128.152.78/',
+  ROOT_API: 'http://10.10.0.146:3000/',
   ROTA_DOMINIO: 'https://agoravaienem.seduc.ro.gov.br/',
+  KEY_CAPTCHA: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
+  // KEY_CAPTCHA: '6Ld93KMZAAAAAFlLG_Us-VvFqaFNRa4TIMPiLB0c',
 };
