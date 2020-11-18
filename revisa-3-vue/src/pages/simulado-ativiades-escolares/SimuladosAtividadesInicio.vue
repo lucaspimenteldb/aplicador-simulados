e66ea895171e94a1a2fc27fee4eedff07eca9e75
larciu <laerciodasilvapedrosa@gmail.com>
2020-11-18 09:30:20 -0300

    Simulado não liberado

--- revisa-3-vue/src/pages/simulado-ativiades-escolares/SimuladosAtividadesInicio.vue
+++ revisa-3-vue/src/pages/simulado-ativiades-escolares/SimuladosAtividadesInicio.vue
@@ -38,7 +38,7 @@
       >
         <v-card
             class="transition rounded__normal cursor__pointer btn__shadow" :class="`destaque__escolares__${area.classe}`"
-            :to="area.url"
+            :to="area.liberado ? area.url : ''"
         >
           <v-card-text>
             <article class="d-flex align-center justify-space-between relative z-1">
@@ -63,7 +63,10 @@ class="font-weight-bold black--text"
             </p>
 
             <!-- prazo de entrega e informações-->
-            <div class="mt-4 d-flex justify-space-between">
+            <div
+class="mt-4 d-flex justify-space-between"
+v-if="area.liberado"
+>
               <article>
                 <p class="black--text">
                   Prazo de entrega:<span class="font-weight-bold black--text">
@@ -74,7 +77,10 @@ class="font-weight-bold black--text"
             </div>
 
             <!-- texto para acessar as atividades novas-->
-            <article class="mt-6 relative">
+            <article
+class="mt-6 relative"
+v-if="area.liberado"
+>
               <p class="font-weight-bold grey--text text--darken-3">
                 {{ `Fazer simulado ${area.titulo}` }}
               </p>
@@ -663,7 +669,6 @@ export default {
       const { id } = this.$store.state.usuario;
       const dados = await this.$http.get(`simulado-estado/novo-simulado/${id}`, { headers: { Authorization: this.$store.state.token } });
       this.preencherSimulados(dados.data.simulados);
-      console.log(dados.data.redacao);
       this.preencherRedacao(dados.data.redacao);
     } catch (e) {
       console.log(e);
@@ -677,7 +682,9 @@ export default {
       for (let i = 0; i < simulados.length; i++) {
         const maps = simulados[i].Areas.map((el) => el.name);
         const url = simulados[i].is_idioma ? `/simulado-responder/${simulados[i].id}/idioma` : `/simulado-responder/${simulados[i].id}`;
+        const liberado = new Date(simulados[i].data_inicio) <= Date.now();
         const beris = {
+          liberado,
           id: simulados[i].id,
           titulo: simulados[i].titulo,
           areas: maps,
@@ -687,6 +694,7 @@ export default {
           situacao: simulados[i].UserSimuladoEstados[0] ? simulados[i].UserSimuladoEstados[0].situacao : 'pendente',
           url,
         };
+
         this.simulados.push(beris);
       }
     },
